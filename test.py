a4b8b0ea45cd2976441994ae909e1971845b248b
caikunkun <mlccnb66@gmail.com>
2024-07-30 13:43:30 +0800

    Save local changes

--- test.py
+++ test.py
@@ -51,31 +51,36 @@ while next_page_link:
     # 查找下一页链接
     next_page_link = soup.find('a', string='下一页', href=True)
 
-# # 打印所有找到的产品页面链接
-for link in product_links:
-    print(link)
-# for link in product_links:
-#     response = requests.get(link)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-    
-#     # 查找所有符合条件的 <div> 标签
-#     div_tags = soup.find_all('div', class_='item-thumbnail')
+# 准备将输出写入到文件中
+output_filename = 'product_details.txt'
 
-#     # 遍历每个符合条件的 <div> 标签
-#     for div_tag in div_tags:
-#         # 在每个 <posts> 标签中查找 <img> <a>标签
-#         img_tags = div_tag.find_all('img')
-#         a_tags = div_tag.find_all('a')
-#         for a in a_tags:
-#             href = a.get('href')  
-#         # 输出每个 <img> 标签的 src\alt 属性值
-#         for img in img_tags:
-#             img_src = img.get('data-src')
-#             img_alt = img.get('alt')
+with open(output_filename, 'w', encoding='utf-8') as f:
+    for link in product_links:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # 查找所有符合条件的 <div> 标签
+        div_tags = soup.find_all('div', class_='item-thumbnail')
+
+        # 遍历每个符合条件的 <div> 标签
+        for div_tag in div_tags:
+            # 在每个 <posts> 标签中查找 <img> <a>标签
+            img_tags = div_tag.find_all('img')
+            a_tags = div_tag.find_all('a')
             
-#             if img_src:
-#                 print(img_alt)
-#             if href:
-#                 print(href)
-#             if img_src:
-#                 print(img_src)
+            # 输出每个 <img> 标签的 src\alt 属性值到文件
+            for img in img_tags:
+                img_src = img.get('data-src')
+                img_alt = img.get('alt')
+                if img_src:
+                    f.write(f"Image Alt: {img_alt}\n")
+                    f.write(f"Image Src: {img_src}\n")
+            # 输出每个 <a> 标签的 href 属性值到文件
+            for a in a_tags:
+                href = a.get('href')
+                if href:
+                    f.write(f"Link Href: {href}\n")
+                    
+            f.write("\n")  # 分隔每个产品的输出
+
+print(f"产品详情已保存到文件: {output_filename}")
